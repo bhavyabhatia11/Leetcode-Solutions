ea1fd6ffa4b1baed67531635c261e1fc7541f037
Bhavya Bhatia <109153562+bhavyabhatia11@users.noreply.github.com>
2023-10-14 15:07:59 +0530

    Time: 19 ms (27.34%), Space: 9.3 MB (31.68%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+
+        dp[0][0] = 0;
+        for(int i=1; i<=m; i++){
+            dp[i][0] = dp[i-1][0] + 1;
+        }
+        for(int i=1; i<=n; i++){
+            dp[0][i] = dp[0][i-1] + 1;
+        }
+
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = min(dp[i][j], dp[i-1][j-1]);
+                }
+                else {
+                    dp[i][j] = min(dp[i][j], dp[i-1][j-1] + 1);
+                }
+                
+            }
+        }
+
+        return dp[m][n];
+        
+    }
+};
